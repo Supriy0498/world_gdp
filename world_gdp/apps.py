d88efc32455a2fdb95a0d6d32341a05cee863bae
Supriy Jinturkar <supriyjinturkar@gmail.com>
2020-11-27 15:56:35 +0530

    updated settings.py app.py

--- world_gdp/apps.py
+++ world_gdp/apps.py
@@ -4,4 +4,4 @@ import pandas as pd
 
 class WorldGdpConfig(AppConfig):
     name = 'world_gdp'
-    randomForest = pickle.load(open('D:\\django venv\\countries_world\\world_gdp\\model\\finalized_model.sav', 'rb'))
+    randomForest = pickle.load(open('/home/worldgdp/world_gdp/world_gdp/model/finalized_model.sav', 'rb'))
